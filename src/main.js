fc65edd0a44382d09bd8b09375b0500b8d6109b5
huanzhen.xu <huanzhen.xu@mi-me.com>
2019-10-30 19:10:55 +0800

    修改公共文件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import Vant from 'vant'
 import App from './App'
 import router from './router.js'
 import api from './common/api.js'
 import utils from './common/utils.js'
+import 'vant/lib/button/style'
 
+import { Field, Button, Toast, Popup, Cell, CellGroup, Icon } from 'vant'
+
+Vue.component(Toast.name, Toast)
+Vue.component(Popup.name, Popup)
+Vue.component(Field.name, Field)
+Vue.component(Button.name, Button)
+Vue.component(Cell.name, Cell)
+Vue.component(CellGroup.name, CellGroup)
+Vue.component(Icon.name, Icon)
+Vue.use(Toast, Popup, Field, Button, Icon)
 
 // 将工具方法绑定到全局
 Vue.prototype.$utils = utils

590443e19a304c1c485c08a1694e18fde1b21910
xujing.jiang <xujing.jiang@xiangme.cn>
2019-11-18 18:39:34 +0800

    联调微信支付

--- src/pages/OrderDetail.vue
+++ src/pages/OrderDetail.vue
@@ -253,10 +253,9 @@
 									"signType": sceneInfo.signType, //微信签名方式：     
 									"paySign": sceneInfo.paySign //微信签名 
 								},
-								
 								function(res) {
+									alert(res)
 									if(res.err_msg == "get_brand_wcpay_request:ok") {
-										alert('到这一步了吗')
 										// 使用以上方式判断前端返回,微信团队郑重提示：
 										//res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
 									}
@@ -265,13 +264,13 @@
 						if(typeof WeixinJSBridge == "undefined") {
 							alert('1111')
 							if(document.addEventListener) {
-								
 								document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
 							} else if(document.attachEvent) {
 								document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
 								document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
 							}
 						} else {
+							alert('222')
 							onBridgeReady();
 						}
 					}

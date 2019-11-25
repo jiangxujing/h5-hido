4b4e06bfe59a2ffdb6b791fbb15754aa01f1d868
xujing.jiang <xujing.jiang@xiangme.cn>
2019-11-25 10:10:55 +0800

    测试问题

--- src/pages/ProductDetail.vue
+++ src/pages/ProductDetail.vue
@@ -221,6 +221,7 @@
 						var nonceStr = res.content.nonceStr;
 						var signature = res.content.signature;
 						var appId = res.content.appid
+						console.log(res)
 						wx.config({
 							debug: false, // 开fff启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 							appId: appId,

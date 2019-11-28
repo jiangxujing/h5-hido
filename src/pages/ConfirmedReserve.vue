415ffa3ce516bab51b86598a894bb66a9fc9c406
huanzhen.xu <huanzhen.xu@mi-me.com>
2019-11-28 16:00:12 +0800

    去除确认到店按钮 增加无折扣券文字

--- src/pages/ConfirmedReserve.vue
+++ src/pages/ConfirmedReserve.vue
@@ -1,45 +1,39 @@
 <template>
     <!-- 预约详情 -->
     <div class="main-content confirmed-reserve">
-        <div class="main-content-request" v-if="request">
-            <div>
-                <div class="detail-content">
-                    <p class="detail-content-item">
-                        <span class="content-item-label">预约项目</span>
-                        <span class="content-item-value">{{orderForm.medicineItemName}}</span>
+        <div v-if="request">
+            <div class="detail-content">
+                <p class="detail-content-item">
+                    <span class="content-item-label">预约项目</span>
+                    <span class="content-item-value">{{orderForm.medicineItemName}}</span>
+                </p>
+                <p class="detail-content-item">
+                    <span class="content-item-label">医师</span>
+                    <span class="content-item-value">{{orderForm.doctorName}}</span>
+                </p>
+                <p class="detail-content-item">
+                    <span class="content-item-label">面诊时间</span>
+                    <span class="content-item-value">{{orderForm.appointmentDate}}</span>
+                </p>
+                <p class="detail-content-item" v-if="orderForm.payAmount">
+                    <span class="content-item-label">预付金</span>
+                    <span class="content-item-value theme-color">{{orderForm.amount}}</span>
+                </p>
+            </div>
+            <div class="detail-item">
+                <p class="detail-item-title">订单信息</p>
+                <div class="detail-info">
+                    <p class="detail-info-item">
+                        <span class="fl-l">订单编号：</span>
+                        <span class="fl-r">{{orderForm.businessNo}}</span>
                     </p>
-                    <p class="detail-content-item">
-                        <span class="content-item-label">医师</span>
-                        <span class="content-item-value">{{orderForm.doctorName}}</span>
-                    </p>
-                    <p class="detail-content-item">
-                        <span class="content-item-label">面诊时间</span>
-                        <span class="content-item-value">{{orderForm.appointmentDate}}</span>
-                    </p>
-                    <p class="detail-content-item" v-if="orderForm.payAmount">
-                        <span class="content-item-label">预付金</span>
-                        <span class="content-item-value theme-color">{{orderForm.amount}}</span>
+                    <p class="detail-info-item">
+                        <span class="fl-l">下单时间：</span>
+                        <span class="fl-r">{{orderForm.createTime}}</span>
                     </p>
                 </div>
-                <div class="detail-item">
-                    <p class="detail-item-title">订单信息</p>
-                    <div class="detail-info">
-                        <p class="detail-info-item">
-                            <span class="fl-l">订单编号：</span>
-                            <span class="fl-r">{{orderForm.businessNo}}</span>
-                        </p>
-                        <p class="detail-info-item">
-                            <span class="fl-l">下单时间：</span>
-                            <span class="fl-r">{{orderForm.createTime}}</span>
-                        </p>
-                    </div>
-                </div>
-                <p class="detail-tips" v-if="orderForm.isToday">温馨提示：请按照预约时间及时到店就诊</p>
             </div>
-            <div class="page-button" v-if="orderForm.isToday">
-                <!-- <van-button class="next-button" @click="confirmPay" :disabled="orderForm.status == '04'">确认到店</van-button> -->
-                <van-button class="next-button" @click="confirmPay" :disabled="orderForm.status !== '01'">确认到店</van-button>
-            </div>
+            <p class="detail-tips">温馨提示：请按照预约时间及时到店就诊</p>
         </div>
     </div>
 </template>
@@ -61,9 +55,7 @@ export default {
                 appointmentDate: '',
                 createTime: '',
                 payAmount: null,
-                deductionAmount: null,
-                isToday: false,
-                status: null
+                deductionAmount: null
             }
         }
     },
@@ -99,25 +91,24 @@ export default {
                                 this.orderForm[key] = content[key]
                             }
                         }
-                        this.orderForm.isToday = new Date(content.appointmentDate).toDateString() === new Date().toDateString() ? true : false
                         this.orderForm.amount = !!content.payAmount && content.payAmount > 0 ? '预付' + this.orderForm.payAmount + '元抵扣' + this.orderForm.deductionAmount + '元' : ''
                     }
                 }
             })
         },
-        // 确认到店
-        confirmPay () {
-            let datas = {
-                businessNo: this.orderForm.businessNo
-            }
-            api.post(api.getUrl('customer-confirmVisit'), datas).then(res => {
-                if (!!res && res.code === 0) {
-                    this.request = true
-                    Toast('确认到店成功')
-                    this.getOrderDetail()
-                }
-            })
-        }
+        // // 确认到店
+        // confirmPay () {
+        //     let datas = {
+        //         businessNo: this.orderForm.businessNo
+        //     }
+        //     api.post(api.getUrl('customer-confirmVisit'), datas).then(res => {
+        //         if (!!res && res.code === 0) {
+        //             this.request = true
+        //             Toast('确认到店成功')
+        //             this.getOrderDetail()
+        //         }
+        //     })
+        // }
     }
 }
 </script>
--- src/pages/CouponList.vue
+++ src/pages/CouponList.vue
@@ -1,22 +1,27 @@
 <template>
     <!-- 我的折扣券 -->
     <div class="main-content coupon-list">
-        <div class="coupon-add" @click="$router.push({name: 'addCoupon'})">
-            <img class="coupon-add-img" src="../assets/images/addbank.png" />创建卡券
-        </div>
-        <div v-for="(item, index) in couponList" :key="index" :class="item.status == '00' || item.status == '01' ? 'coupon-item' : 'coupon-item coupon-item-over'">
-            <div class="coupon-item-left">
-                <div>
-                    <p class="coupon-item-amount DINAlternate-Bold"><span>￥</span>{{item.amountShow}}</p>
-                    <p>{{item.statusShow}}</p>
+        <div v-if="request">
+            <div class="coupon-add" @click="$router.push({name: 'addCoupon'})">
+                <img class="coupon-add-img" src="../assets/images/addbank.png" />创建卡券
+            </div>
+            <div v-if="!!couponList && couponList.length > 0" >
+                <div v-for="(item, index) in couponList" :key="index" :class="item.status == '00' || item.status == '01' ? 'coupon-item' : 'coupon-item coupon-item-over'">
+                    <div class="coupon-item-left">
+                        <div>
+                            <p class="coupon-item-amount DINAlternate-Bold"><span>￥</span>{{item.amountShow}}</p>
+                            <p>{{item.statusShow}}</p>
+                        </div>
+                    </div>
+                    <div class="coupon-item-content">
+                        <p>{{item.deductionAmountShow + '元抵扣券'}}</p>
+                        <p class="coupon-item-info">{{'使用人:' + item.mobile}}</p>
+                        <p class="coupon-item-info">{{'有效期至' + item.expiryTime}}</p>
+                    </div>
+                    <div class="close-btn" @click="closeCoupon(item)" v-if="item.status == '00' || item.status == '01'">取消</div>
                 </div>
             </div>
-            <div class="coupon-item-content">
-                <p>{{item.deductionAmountShow + '元抵扣券'}}</p>
-                <p class="coupon-item-info">{{'使用人:' + item.mobile}}</p>
-                <p class="coupon-item-info">{{'有效期至' + item.expiryTime}}</p>
-            </div>
-            <div class="close-btn" @click="closeCoupon(item)" v-if="item.status == '00' || item.status == '01'">取消</div>
+            <div class="no-content" v-else>暂无折扣券</div>
         </div>
     </div>
 </template>
@@ -30,6 +35,7 @@ export default {
     name: 'coupon-list',
     data () {
         return {
+            request: false,
             couponList: [],
             statusList: [{
                 value: '00',
@@ -62,6 +68,7 @@ export default {
         getCoupopList () {
             api.post(api.getUrl('agent-myCoupon'), {}).then(res => {
                 if (!!res && res.code === 0) {
+                    this.request = true
                     if (!!res.content && res.content.length > 0) {
                         this.couponList = res.content.map(item => {
                             let data = {}

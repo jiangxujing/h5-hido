ba63b4b0453a675332bab6f9240f0f1033e83684
huanzhen.xu <huanzhen.xu@mi-me.com>
2019-12-02 17:45:02 +0800

    我的报告联调

--- src/pages/ReportList.vue
+++ src/pages/ReportList.vue
@@ -44,52 +44,20 @@ export default {
     methods: {
         // 获取报告列表
         getReportList () {
-            // api.post(api.getUrl('customer-myReports'), {}).then(res => {
-            //     if (!!res && res.code === 0) {
-            //         this.request = true
-            //         if (!!res.content && res.content.length > 0) {
-            //             this.reportList = res.content.map(item => {return item})
-            //         } else {
-            //             this.reportList = []
-            //         }
-            //     }
-            // })
-            let res = {
-                content: []
-            }
-            res.content = [{
-                reportUrl: 'kfb.mxfvip.com/upload/case/47593/c_1564401361.html',
-                reportTitle: '长期 头孢 静脉 脂肪',
-                reportTime: '2019-07-29 19:56:03'
-            
-            }, {
-                reportUrl: 'kfb.mxfvip.com/upload/case/47593/c_1564401358.html',
-                reportTitle: '临时 头孢 静脉 脂肪',
-                reportTime: '2019-07-29 19:56:51'
-            }, {
-                reportUrl: 'kfb.mxfvip.com/upload/case/47593/c_1564401365.html',
-                reportTitle: '体温单',
-                reportTime: '2019-07-29 19:57:50'
-            }, {
-                reportUrl: 'kfb.mxfvip.com/upload/case/47593/c_1564401374.html',
-                reportTitle: '手术患者与病房交接记录单',
-                reportTime: '2019-07-29 19:59:09'
-            }]
-            this.reportList = res.content.map(item => {return item})
-            this.request = true
+            api.post(api.getUrl('customer-myReports'), {}).then(res => {
+                if (!!res && res.code === 0) {
+                    this.request = true
+                    if (!!res.content && res.content.length > 0) {
+                        this.reportList = res.content.map(item => {return item})
+                    } else {
+                        this.reportList = []
+                    }
+                }
+            })
         },
         // 跳转报告详情
         openDetail (item) {
             if (!item.reportUrl) {return false}
-            // if (!item.reportNo) {return false}
-            // let query = {
-            //     reportNo: item.reportNo
-            // }
-            // let pageName = '/reportDetail'
-            // this.$router.push({
-            //     path: pageName,
-            //     query: query
-            // })
             window.location.href = item.reportUrl
         }
     }

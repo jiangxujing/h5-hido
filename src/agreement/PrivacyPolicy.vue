eaba43c80b3db9f30fc2d5ccb8a072c26917b3cb
huanzhen.xu <huanzhen.xu@mi-me.com>
2019-12-11 14:12:01 +0800

    调整协议样式

--- src/agreement/PrivacyPolicy.vue
+++ src/agreement/PrivacyPolicy.vue
@@ -195,40 +195,5 @@ export default {
 </script>
 
 <style lang="scss" scope>
-	.agreement {
-		background-color: #F5F5F5;
-		padding-top: 1rem;
-		min-height: 100%;
-		.agreement-content {
-			width: 100%;
-			height: 100%;
-			background-color:#fff;
-			padding: 8px 20px 28px;
-		}
-		p {
-			margin-bottom: 6px;
-		}
-		.agreement-title {
-			color: #353535;
-			font-size: 18px;
-			font-weight: 700;
-			line-height: 50px;
-			text-align: center;
-		}
-		.agreement-main {
-			font-size: 14px;
-			line-height: 24px;
-			color:#4B464D;
-		}
-		.agreement-subtitle {
-			margin-bottom: 10px;
-		}
-		.weight {
-			font-weight:700;
-			color: #353535!important;
-		}
-		.text-indent-2em {
-			text-indent: 2em;
-		}
-	}
+
 </style>
